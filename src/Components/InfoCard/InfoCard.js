d937367c9443d2da78c1b4ca2d0aaaa97217c903
Asraful Islam <iasraful321@gmail.com>
2021-09-01 01:01:54 +0600

    info section updated

--- src/Components/InfoCard/InfoCard.js
+++ src/Components/InfoCard/InfoCard.js
@@ -8,11 +8,7 @@ const InfoCard = props => {
     return (
         <div className="col-md-4 text-white mb-2">
             <div className={`${bg} single-info  py-4  px-5 bg-primary  d-flex align-items-center rounded`}>
-                <FontAwesomeIcon className="icon mr-4" icon={
-                        icon === 'clock' ? faClock :
-                        icon === 'location' ? faMapMarkerAlt :
-                        faPhoneAlt
-                } />
+                
                 <div>
                     <h6>{title}</h6>
                     <p className="small m-0">{description}</p>
